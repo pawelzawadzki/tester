26b6b9ce56e35f8311601cd7be216fd07cf08609
pawelzawadzki <pablop7@wp.pl>
2017-03-20 23:55:36 +0100

    przeniesienie plików
    przeniesienie plików

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Tester1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOptionA = new System.Windows.Forms.TextBox();
+            this.txtOptionB = new System.Windows.Forms.TextBox();
+            this.txtOptionC = new System.Windows.Forms.TextBox();
+            this.txtOptionD = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pytanie:";
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(98, 57);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(372, 20);
+            this.txtTask.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "C:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "D:";
+            // 
+            // txtOptionA
+            // 
+            this.txtOptionA.Location = new System.Drawing.Point(98, 108);
+            this.txtOptionA.Name = "txtOptionA";
+            this.txtOptionA.Size = new System.Drawing.Size(310, 20);
+            this.txtOptionA.TabIndex = 6;
+            // 
+            // txtOptionB
+            // 
+            this.txtOptionB.Location = new System.Drawing.Point(98, 156);
+            this.txtOptionB.Name = "txtOptionB";
+            this.txtOptionB.Size = new System.Drawing.Size(310, 20);
+            this.txtOptionB.TabIndex = 7;
+            // 
+            // txtOptionC
+            // 
+            this.txtOptionC.Location = new System.Drawing.Point(98, 199);
+            this.txtOptionC.Name = "txtOptionC";
+            this.txtOptionC.Size = new System.Drawing.Size(310, 20);
+            this.txtOptionC.TabIndex = 8;
+            // 
+            // txtOptionD
+            // 
+            this.txtOptionD.Location = new System.Drawing.Point(98, 240);
+            this.txtOptionD.Name = "txtOptionD";
+            this.txtOptionD.Size = new System.Drawing.Size(310, 20);
+            this.txtOptionD.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "NEXT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "PREVIOUS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 322);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOptionD);
+            this.Controls.Add(this.txtOptionC);
+            this.Controls.Add(this.txtOptionB);
+            this.Controls.Add(this.txtOptionA);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tester - Daj się poznać 2017";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtOptionA;
+        private System.Windows.Forms.TextBox txtOptionB;
+        private System.Windows.Forms.TextBox txtOptionC;
+        private System.Windows.Forms.TextBox txtOptionD;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tester1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        DatabaseConnection objConnect;
+        string conString;
+
+        DataSet ds;
+
+        DataRow dRow;
+
+        int MaxRows;
+        int inc = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                objConnect = new DatabaseConnection();
+                conString = Properties.Settings.Default.QuestConnectionString;
+                objConnect.connection_string = conString;
+
+                objConnect.Sql = Properties.Settings.Default.SQL;
+                ds = objConnect.GetConnection;
+
+                MaxRows = ds.Tables[0].Rows.Count;
+                NavigateRecords();
+
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message);
+
+            }
+        }
+
+        private void NavigateRecords()
+        {
+
+            dRow = ds.Tables[0].Rows[inc];
+
+            txtTask.Text = dRow.ItemArray.GetValue(1).ToString();
+            txtOptionA.Text = dRow.ItemArray.GetValue(2).ToString();
+            txtOptionB.Text = dRow.ItemArray.GetValue(3).ToString();
+            txtOptionC.Text = dRow.ItemArray.GetValue(4).ToString();
+            txtOptionD.Text = dRow.ItemArray.GetValue(5).ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (inc < MaxRows - 1)
+            {
+
+                inc++;
+                NavigateRecords();
+
+            }
+            else
+            {
+
+                inc = 0;
+                NavigateRecords();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (inc > 0)
+            {
+
+                inc--;
+                NavigateRecords();
+
+            }
+            else
+            {
+
+                inc = MaxRows - 1;
+                NavigateRecords();
+
+            }
+        }
+    }
+}
